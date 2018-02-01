27e84bed201971679084561a27df28432fca8d68
Anatoly <iseekyouu@yandex.ru>
2018-02-01 16:51:06 +0700

    final check

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -9,18 +9,16 @@ import { Grid, Row, Col, FormGroup, ControlLabel, FormControl,
 //http://theory.phphtml.net
 
 class TodoForm extends React.Component{
-   /*constructor(props){
-        super(props);
-        this.state = this.props.list;
-    }*/
-    handleChange(name){
+    /**
+     * onChange form element handle, experiment
+     * @params key {string} chenged key
+     * */
+    handleChange(key){
         return(
             (event) =>
-                this.props.onChange(this.props.index, name, event)
+                this.props.onChange(this.props.index, key, event)
         )
     }
-
-
 
     renderFormTextInput(key){
         return(
@@ -84,7 +82,10 @@ class TodoForm extends React.Component{
           />
       )
     }
-
+    /**
+     * Form element render, why not, experimental
+     * @params element {object: {key:, name:, type:}}
+     * */
     renderFormElement(element){
         const types = {
             input: this.renderFormTextInput,
@@ -94,10 +95,13 @@ class TodoForm extends React.Component{
             checkbox: this.renderFormCheckBox
         };
 
-        if (!types[element.type]) return null
+        if (!types[element.type]) return null;
+
+        /// experimental
         const handle = types[element.type].bind(this, element.key)();
 
-        const label = element.nolabel ? null : [element].map((el) => {
+        /// experimental
+        const label = [element].map((el) => {
             return (<ControlLabel key={el.name}>{el.name}</ControlLabel>)
           }
         )
@@ -111,93 +115,104 @@ class TodoForm extends React.Component{
     }
 
     render(){
-        if (!this.props.list) return null
+        if (!this.props.list) return null;
 
         return(
-            <Grid>
-              <Row>
-                <Col xs={6} md={6}>
-                  <form>
-                      {this.renderFormElement({key: 'name', name: 'Название', type: 'input'})}
-                      {this.renderFormElement({key: 'descr', name: 'Описание', type: 'textarea'})}
-                      {this.renderFormElement({key: 'priority', name: 'Приоритет', type: 'select'})}
-                      {this.renderFormElement({key: 'deadline', name: 'Срок выполнения', type: 'date'})}
-                      {this.renderFormElement({key: 'enddate', name: 'Дата завершения', type: 'date'})}
-                      {this.renderFormElement({key: 'done', name: 'Выполнено', type: 'checkbox'})}
-                  </form>
-                </Col>
-              </Row>
-            </Grid>
-        )
+            <form>
+                {this.renderFormElement({key: 'name', name: 'Название', type: 'input'})}
+                {this.renderFormElement({key: 'descr', name: 'Описание', type: 'textarea'})}
+                {this.renderFormElement({key: 'priority', name: 'Приоритет', type: 'select'})}
+                {this.renderFormElement({key: 'deadline', name: 'Срок выполнения', type: 'date'})}
+                {this.renderFormElement({key: 'enddate', name: 'Дата завершения', type: 'date'})}
+                {this.renderFormElement({key: 'done', name: 'Выполнено', type: 'checkbox'})}
+            </form>
+         )
     }
 }
+
 class Filters extends React.Component{
+
   render(){
     return (
-        <Grid>
-          <Row>
-            <Col xs={8} md={8}>
-              <ButtonGroup>
-                <Button>All</Button>
-                <Button>Low</Button>
-                <Button>Mid</Button>
-                <Button>High</Button>
-              </ButtonGroup>
-            </Col>
-          </Row>
-        </Grid>
-
+      <ButtonGroup>
+        <Button onClick={() => this.props.filter('all')}>All</Button>
+        <Button onClick={() => this.props.filter('low')}>Low</Button>
+        <Button onClick={() => this.props.filter('mid')}>Mid</Button>
+        <Button onClick={() => this.props.filter('high')}>High</Button>
+      </ButtonGroup>
     )
   }
 }
 
 class Game extends React.Component {
     constructor(){
-        super();
-        this.state = {
-            list: [
-                {
-                    name: 'hello',
-                    descr: 'description',
-                    priority: 1,
-                    deadline:'2018-01-19',
-                    enddate: formatDate(new Date()),
-                    done: 0
-                }
+      super();
 
-            ],
+      window.onunload = window.onclose = function onExit() {
+        localStorage.setItem('todo-list', JSON.stringify(this.state.list));
+      }.bind(this);
+
+      let list = localStorage.getItem('todo-list');
+
+      /// try to get data from localStorage, else set empty template
+      try {
+        list = JSON.parse(list);
+        if (!typeof list === 'array') throw 'bad'
+      } catch (e){
+        list = [{
+          name: "",
+          descr: "",
+          priority: 0,
+          deadline: "",
+          enddate: "",
+          done: 0
+        }];
+      }
+
+      this.state = {
+            list: list,
             currentIndex: 0,
-            hideForm: 0
+            hideForm: 0,
+            filter: 'all'
         }
     }
+    /**
+     * Simple function for priority name
+     * @params priority {int} code of priority
+     * @returns {String}
+     * */
+    getPriorityName(priority) {
+      const p = {
+        0: 'low',
+        1: 'mid',
+        2: 'high'
+      };
 
-    getPriorityName(priority){
-       switch (priority){
-        case 0:
-          return 'low';
-        case 1:
-          return 'mid'
-        case 2:
-          return 'high'
-        default:
-          return 'low'
-      }
+      return p[priority];
     }
 
-    render() {
-        const list = this.state.list.map((item, index) => {
-            const active = index === this.state.currentIndex ?
-                'active': '';
-            const done = item.done ? 'done' : ''
-            const className = `${active} ${done}`;
-            const priorityName = this.getPriorityName(item.priority)
-            return (
+    /**
+     * Todo-list render
+     * */
+    renderList(){
+      return (
+        this.state.list.map((item, index) => {
+        const active = index === this.state.currentIndex ?
+            'active': '';
+        const done = item.done ? 'done' : '';
+        const className = `${active} ${done}`;
+        const priorityName = this.getPriorityName(item.priority);
+
+        //console.log(item, item.priority);
+
+        if (!this.filter(index)) return false;
+          return (
               <li
-                key={index}
-                className={className}
-                onClick={this.setCurrentIndex.bind(this, index)}
+                  key={index}
+                  className={className}
+                  onClick={this.setCurrentIndex.bind(this, index)}
               >
-                <dl className="dl-horizontal">
+                <dl className={this.outOfTime(index)}>
                   <dt>Название</dt>
                   <dd>{item.name}</dd>
                   <dt>Описание</dt>
@@ -211,70 +226,85 @@ class Game extends React.Component {
                   <dt>Удалить</dt>
                   <dd>
                     <Button bsStyle="danger"
-                            onClick={(event) => this.handleRemove(this.state.currentIndex, event)}
+                            onClick={(event) => this.handleRemove(index, event)}
                     >
                       Remove current
                     </Button>
                   </dd>
                 </dl>
-            </li>);
-        });
+              </li>);
+        })
+      )
+    }
 
+    render() {
+        const list = this.renderList();
         const index = this.state.currentIndex;
         return (
-            <div className="todo">
-                <div className="todo-board">
-                    <Grid>
-                      <Row>
-                        <Col xs={8} md={8}>
-                          <Button bsStyle="success"
-                              onClick={this.handleAdd.bind(this)}
-                          >Add
-                          </Button>
-                          <Button bsStyle="success"
-                                  onClick={() =>
-                                  this.setState({hideForm: !this.state.hideForm})}
-                          >Toggle form visibility
-                          </Button>
-                        </Col>
-                      </Row>
-                    </Grid>
-
-                    <Filters />
-
-                    {
-                      !this.state.hideForm ?
-                          <TodoForm
-                            list={this.state.list[index]}
-                            index={index}
-                            onChange={this.handleChange.bind(this)}
-                          /> : null
-                    }
-
-                </div>
-                <Grid className="game-info">
-                  <Row>
-                    <Col xs={8} md={8}>
-                      <ol>{list}</ol>
-                    </Col>
-                  </Row>
-                </Grid>
-            </div>
+          <Grid >
+            <Row>
+              <Col xs={4} md={4} className="sticky">
+                <Button bsStyle="success"
+                    onClick={this.handleAdd.bind(this)}
+                >Add
+                </Button>
+                <Button bsStyle="success"
+                        onClick={() =>
+                        this.setState({hideForm: !this.state.hideForm})}
+                >Toggle form visibility
+                </Button>
+                <Filters filter={this.setFilter.bind(this)} />
+                {
+                  !this.state.hideForm ?
+                      <TodoForm
+                          list={this.state.list[index]}
+                          index={index}
+                          onChange={this.handleChange.bind(this)}
+                      /> : null
+                }
+              </Col>
+              <Col xs={6} md={6} className="game-info">
+                <ol>{list}</ol>
+              </Col>
+            </Row>
+          </Grid>
         );
+    }
+
+    /**
+     * Marks out of time items
+     * @params index {int} current item index
+     * @returns className {string}
+     * */
+    outOfTime(index){
+      let current = this.state.list[index];
+      let className = 'dl-horizontal';
+
+      if (current.deadline < current.enddate) {
+        className +=' text-danger'
+      }
+
+      return (className);
     }
 
     setCurrentIndex(index){
         this.setState({currentIndex: index});
     }
 
+    /**
+     * Remove item handle
+     * @params index {int} current item index
+     * @params event {event}
+     * */
     handleRemove(index, event) {
+      /// stops click!
       event.stopPropagation();
 
       if (!confirm("Are u sure?")) return false;
 
       this.state.list.splice(index, 1);
       let newIndex = --this.state.currentIndex;
-      newIndex = index < 0 ? 0 : newIndex;
+      newIndex = newIndex < 0 ? 0 : newIndex;
 
       this.setState({
         list: this.state.list,
@@ -283,6 +313,9 @@ class Game extends React.Component {
 
     }
 
+    /**
+     * Add new item in todo-list
+     * */
     handleAdd(){
         const index = this.state.list.length ? ++this.state.currentIndex : 0;
         this.setState(
@@ -301,13 +334,22 @@ class Game extends React.Component {
         )
     }
 
+    /**
+     * Form elements change handle
+     * @params index {int} current item index
+     * @params field {string} key of item
+     * @params event {event}
+     * */
     handleChange(index, field, event){
         let value;
+
+        /// for checkbox
         if (field === 'done') {
           value = event.target.checked ? 1 : 0
         } else {
           value = event.target.value
         }
+
         const item = this.state.list[index];
         item[field] = value;
         this.state.list[index] = item;
@@ -316,15 +358,41 @@ class Game extends React.Component {
             list: this.state.list
         });
     }
+
+    /**
+    * sets filter type all/low/mid/high
+     * @params type {string} filter name
+    * */
+    setFilter(type){
+      this.setState({filter: type});
+    }
+
+    /**
+     * filter on list, check current item
+     * @params index {int} current index
+     * @returns result, true/false
+     * */
+    filter(index){
+      const current = this.state.list[index];
+      const filter = this.state.filter;
+      let result;
+
+      const filters = {
+        all: () => true,
+        low: () => current.priority == 0,
+        mid: () => current.priority == 1,
+        high: () => current.priority == 2
+      };
+
+      result = filters[filter]();
+      //console.log(result);
+
+      return (result);
+    }
 }
-
-
-function formatDate(date){
-  return date.toISOString().split('T')[0]
-}
-
 
 // ========================================
+
 ReactDOM.render(
     <Game />,
     document.getElementById('root')
